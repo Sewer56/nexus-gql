bdbe00198314e16f83414bff11627fb2eb62bc71
Sewer56 <sewer56lol@googlemail.com>
2025-06-29 23:18:09 +0100

    Initial Repository Files

---
+++ projects/batch-downloader-cli/src/main.rs
@@ -0,0 +1,7 @@
+use nexus_gql;
+
+fn main() {
+    println!("Batch Downloader CLI - Nexus Mods GraphQL");
+    println!("This is a placeholder CLI application.");
+    // TODO: Implement CLI functionality
+}
